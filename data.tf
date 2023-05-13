8e0fb2a5038d6e70b28f92f9395e4d87fa9d828e
kayoucarolle <carollematchum@gmail.com>
2023-05-12 20:08:42 -0500

    add postgre module

---
+++ data.tf
@@ -0,0 +1,20 @@
+data "aws_vpc" "vpc" {
+  filter {
+    name   = "tag:Name"
+    values = ["2560-dev-alpha-vpc"]
+  }
+}
+
+data "aws_subnet" "subnet_01" {
+  filter {
+    name   = "tag:Name"
+    values = ["2560-dev-alpha-subnetweb1a"]
+  }
+}
+
+data "aws_subnet" "subnet_02" {
+  filter {
+    name   = "tag:Name"
+    values = ["2560-dev-alpha-subnetweb1b"]
+  }
+}
